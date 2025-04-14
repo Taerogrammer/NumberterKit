2bd802f152938fe6f46d155fddc185703faa0f8b
Taerogrammer <hyung54321@naver.com>
2025-04-14 15:56:35 +0900

    feat: Decimal+Formatted 추가

--- Sources/NumberterKit/Core/FormatterProvider.swift
+++ Sources/NumberterKit/Core/FormatterProvider.swift
@@ -9,6 +9,18 @@ import Foundation
 
 public enum FormatterProvider {
 
+    /// 지정된 소수점 자리수를 가진 NumberFormatter를 반환합니다.
+    public static func decimal(fractionDigits: Int) -> NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = fractionDigits
+        formatter.maximumFractionDigits = fractionDigits
+        formatter.groupingSeparator = ","
+        formatter.locale = Locale(identifier: "ko_KR")
+
+        return formatter
+    }
+
     /// 쉼표만 포함, 소수점 없음
     public static let integerDecimal: NumberFormatter = {
         let formatter = NumberFormatter()
